e7045043e09873853da449ad66fa04e9da91524f
Britannio Jarrett <britanniojarrett@gmail.com>
2022-01-17 15:44:53 +0000

    define the grid and the next player

--- main.hs
+++ main.hs
@@ -1,3 +1,21 @@
 import Data.Char ()
 import Data.List ()
 import System.IO ()
+
+-- Size of the tic-tac-toe grid
+size :: Integer
+size = 3
+
+-- a NxN grid of Player values
+type Grid = [[Player]]
+
+data Player
+  = O -- nought
+  | B -- blank
+  | X -- cross
+  deriving (Eq, Ord, Show)
+
+next :: Player -> Player
+next O = X
+next B = B
+next X = O
